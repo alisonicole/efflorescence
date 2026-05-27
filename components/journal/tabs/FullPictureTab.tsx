7f3a263c5b7a7f7b5edf7892f1001d29f502b422
Alison Lee <lee.alisonnicole@bcg.com>
2026-05-26 22:41:17 -0400

    feat(journal): add FullPictureTab component

---
+++ components/journal/tabs/FullPictureTab.tsx
@@ -0,0 +1,9 @@
+import FullPicture from "@/components/journal/FullPicture";
+
+export default function FullPictureTab() {
+  return (
+    <div className="pt-2 pb-4 px-2.5">
+      <FullPicture />
+    </div>
+  );
+}
