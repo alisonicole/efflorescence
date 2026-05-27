0f7301c1c2b410ea02da66273b203f7e850263df
Alison Lee <lee.alisonnicole@bcg.com>
2026-05-26 22:40:06 -0400

    fix(journal): hoist DiaryTab excludeTypes to module scope

--- components/journal/tabs/DiaryTab.tsx
+++ components/journal/tabs/DiaryTab.tsx
@@ -4,16 +4,21 @@ import { useState } from "react";
 import EntryEditor from "@/components/journal/EntryEditor";
 import EntryList from "@/components/journal/EntryList";
 
+type ExcludableType = "standard" | "rewrite" | "receipts" | "the_why";
+
+const DIARY_EXCLUDE_TYPES: ExcludableType[] = [
+  "rewrite",
+  "receipts",
+  "the_why",
+];
+
 export default function DiaryTab() {
   const [refreshKey, setRefreshKey] = useState(0);
 
   return (
     <div className="space-y-2.5 pb-4">
       <EntryEditor prompt="" onSaved={() => setRefreshKey((k) => k + 1)} />
-      <EntryList
-        excludeTypes={["rewrite", "receipts", "the_why"]}
-        refreshKey={refreshKey}
-      />
+      <EntryList excludeTypes={DIARY_EXCLUDE_TYPES} refreshKey={refreshKey} />
     </div>
   );
 }
